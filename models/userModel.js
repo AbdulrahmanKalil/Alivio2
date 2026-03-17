efbc4226d80c683b87f30b2c788aad1ec3dd9d41
AbdulrahmanKalil <Khalilabdulrahman92@gmail.com>
2026-03-17 10:34:57 +0200

    ready for deployment

--- models/userModel.js
+++ models/userModel.js
@@ -64,8 +64,6 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 12);
 
   this.passwordConfirm = undefined;
-
-  next();
 });
 
 userSchema.methods.correctPassword = async function (
