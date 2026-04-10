7be57874546f605beb24b50e3ccf4a5360c3e08c
AbdulrahmanKalil <Khalilabdulrahman92@gmail.com>
2026-04-11 01:02:49 +0200

    fix: correct controller import case

--- routes/prescriptionRoutes.js
+++ routes/prescriptionRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const authController = require("../controllers/authController");
 const { protect, restrictTo } = require("../middlewares/authMiddleware");
 const prescriptionController = require("../controllers/prescriptionController");
-
 const router = express.Router();
 
 router
